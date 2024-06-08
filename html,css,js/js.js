d7ddd4b5e8857265fc90e2f472316117cc6e9254
Muhammad Ahmed <muhammadahmedmughal24@gmail.com>
2024-06-08 16:09:29 +0500

    all

---
+++ html,css,js/js.js
@@ -0,0 +1,69 @@
+// let box = document.querySelector("div"),
+// input = document.querySelector("input");
+// input.addEventListener("input", () =>{
+//     box.style.borderRadius = input.value;
+//     box.style.background = input.value;
+//     box.style.borderColor = input.value;
+// });
+
+// // // function data(){
+// // //     for (let i = Math.ceil(i*(Math.random)); i>=i) {
+// // //         const element = array[i];
+        
+// // //     }
+// //     alert(`this is your lucky number`)
+// //   }
+
+
+
+// // const hola = () => alert('hola, Mundo');
+
+// const userQue = prompt('Give me a No.')
+// function userQue(n){
+//     return userQue*n;
+// }
+// console.log(userQue(3));
+
+// const names = [];
+
+// let questions = ['Name','school Name','total Marks','obtained Marks'];
+// let answers = [];
+// let counter = 0;
+// while(counter < questions.length ){
+//     let answer = prompt(`What is your ${questions[counter]}?`);
+//     answers.push(answer.trim().toLowerCase());
+//     counter++;
+// }
+// document.write("<h1> Mark Sheet </h1>")
+// var names = prompt("what is your Name?");
+// document.write("Student Name =" + names + "<br>");
+// var schoolName = prompt("What is your School Name?");
+// document.write("School Name=" + schoolName + "<br>");
+// var totalMarks = prompt("What is your Total Marks?");
+// document.write("Total Marks=" + totalMarks + "<br>");
+// var obtainedMarks = prompt("What is your Obtaiened Marks?");
+// document.write("Obtained Marks=" + obtainedMarks + "<br>");
+
+// var percentage = (obtainedMarks / totalMarks) * 100;
+// document.write("Percentage = " + percentage.toFixed(2) + "%" + "<br>");
+
+// //const redButton = document.getElementById('red');
+// const greenButton = document.getElementById('green');
+
+
+
+
+// redButton.addEventListener('click',
+//  e => document.body.style.background = 'red');
+
+
+//  greenButton.addEventListener('click', 
+//  (e) => {
+//     console.log('event',e);
+//     document.body.style.background = e.target.textContent
+//  //}
+
+// const searchInput = document.getElementById('search');
+// const searchBtn = document.getElementById('searchButton');
+
+// searchbtm=
